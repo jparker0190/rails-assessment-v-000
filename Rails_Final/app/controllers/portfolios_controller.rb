7ecb397396dd1f194c72162dc46ffb3ccc835812
Joseph Parker <jparker010190@gmail.com>
2018-08-30 03:07:10 +0000

    adding iex function

--- Rails_Final/app/controllers/portfolios_controller.rb
+++ Rails_Final/app/controllers/portfolios_controller.rb
@@ -61,7 +61,11 @@ class PortfoliosController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  def iex
+    @resp = Faraday.get 'https://api.iextrading.com/1.0/tops'
+    @body_hash = JSON.parse(@resp.body)
+    render 'index'
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_portfolio
